37a987b0781a7dd62fcc86ae406c050d34361130
Daniel Mendler <mail@daniel-mendler.de>
2016-09-08 01:30:25 +0200

    initial commit

---
+++ Control/Monad/RWS/CPS.hs
@@ -0,0 +1,69 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Monad.RWS.CPS
+-- Copyright   :  (c) Daniel Mendler 2016,
+--                (c) Andy Gill 2001,
+--                (c) Oregon Graduate Institute of Science and Technology, 2001
+-- License     :  BSD-style (see the file LICENSE)
+--
+-- Maintainer  :  mail@daniel-mendler.de
+-- Stability   :  experimental
+-- Portability :  non-portable (type families)
+--
+-- Stricter RWS monad using continuation-passing-style for the
+-- writer output.
+--
+--      Inspired by the paper
+--      /Functional Programming with Overloading and Higher-Order Polymorphism/,
+--        Mark P Jones (<http://web.cecs.pdx.edu/~mpj/>)
+--          Advanced School of Functional Programming, 1995.
+-----------------------------------------------------------------------------
+
+module Control.Monad.RWS.CPS (
+  -- * The RWS monad
+  RWS,
+  rws,
+  runRWS,
+  evalRWS,
+  execRWS,
+  mapRWS,
+  withRWS,
+  -- * The RWST monad transformer
+  RWST,
+  runRWST,
+  evalRWST,
+  execRWST,
+  mapRWST,
+  withRWST,
+  -- * Strict Reader-writer-state monads
+  module X
+) where
+
+import Control.Monad as X
+import Control.Monad.Fix as X
+import Control.Monad.RWS.Class as X
+import Control.Monad.Trans as X
+import Data.Monoid as X
+
+import Control.Monad.Trans.RWS.CPS
+import qualified Control.Monad.Trans.RWS.CPS as CPS
+
+-- Orphan instance
+instance (Monoid w, Monad m) => MonadWriter (RWST r w s m) where
+  type WriterType (RWST r w s m) = w
+  tell = CPS.tell
+  listen = CPS.listen
+  pass = CPS.pass
+
+-- Orphan instance
+instance Monad m => MonadReader (RWST r w s m) where
+  type EnvType (RWST r w s m) = r
+  ask = CPS.ask
+  local = CPS.local
+
+-- Orphan instance
+instance Monad m => MonadState (RWST r w s m) where
+  type StateType (RWST r w s m) = s
+  get = CPS.get
+  put = CPS.put
---
+++ Control/Monad/Writer/CPS.hs
@@ -0,0 +1,51 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Monad.Writer.CPS
+-- Copyright   :  (c) Daniel Mendler 2016,
+--                (c) Andy Gill 2001,
+--                (c) Oregon Graduate Institute of Science and Technology, 2001
+-- License     :  BSD-style (see the file LICENSE)
+--
+-- Maintainer  :  mail@daniel-mendler.de
+-- Stability   :  experimental
+-- Portability :  non-portable (type families)
+--
+-- Stricter writer monad using continuation-passing-style for the
+-- writer output.
+--
+--      Inspired by the paper
+--      /Functional Programming with Overloading and Higher-Order Polymorphism/,
+--        Mark P Jones (<http://web.cecs.pdx.edu/~mpj/pubs/springschool.html>)
+--          Advanced School of Functional Programming, 1995.
+-----------------------------------------------------------------------------
+
+module Control.Monad.Writer.CPS (
+  -- * The Writer monad
+  Writer,
+  runWriter,
+  execWriter,
+  mapWriter,
+  -- * The WriterT monad transformer
+  WriterT,
+  runWriterT,
+  execWriterT,
+  mapWriterT,
+  module X
+) where
+
+import Control.Monad as X
+import Control.Monad.Fix as X
+import Control.Monad.Trans as X
+import Control.Monad.Writer.Class as X
+import Data.Monoid as X
+
+import Control.Monad.Trans.Writer.CPS
+import qualified Control.Monad.Trans.Writer.CPS as CPS
+
+-- Orphan instance
+instance (Monoid w, Monad m) => MonadWriter (WriterT w m) where
+  type WriterType (WriterT w m) = w
+  tell = CPS.tell
+  listen = CPS.listen
+  pass = CPS.pass
